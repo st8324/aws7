112579ccfbc38d40a60a2073ad862da924c8c49a
st8324 <stajun@kut.ac.kr>
2026-07-21 17:06:34 +0900

    자바 02일차 : if문 예제

---
+++ java_jik/src/day02/Ex10_if문연습예제.java
@@ -0,0 +1,41 @@
+package day02;
+
+import java.util.Scanner;
+
+public class Ex10_if문연습예제 {
+
+	public static void main(String[] args) {
+		/* Scanner를 이용하여 정수를 입력받아 정수가 양수인지, 0인지, 음수인지를 판별하는 코드를 작성하세요.
+		 * 예시1
+		 * 정수 입력 : 10
+		 * 10은 양수
+		 * 
+		 * 예시2
+		 * 정수 입력 : -3
+		 * -3은 음수
+		 * 
+		 * 예시3
+		 * 정수 입력 : 0
+		 * 0
+		 * */
+		
+		//정수 num 입력
+		Scanner scan = new Scanner(System.in);
+		System.out.print("정수 입력 : ");
+		int num = scan.nextInt();
+		
+		//num이 0보다 크면 양수라고 콘솔에 출력하고
+		if(num > 0) {
+			System.out.println(num + "는 양수");
+		}
+		//num이 0보다 작으면 음수라고 콘솔에 출력하고
+		else if(num < 0) {
+			System.out.println(num + "는 음수");
+		}
+		//아니면 0이라고 출력하세요.
+		else {
+			System.out.println(num);
+		}
+	}
+
+}
---
+++ java_jik/src/day02/Ex11_if문연습예제2.java
@@ -0,0 +1,62 @@
+package day02;
+
+import java.util.Scanner;
+
+public class Ex11_if문연습예제2 {
+
+	public static void main(String[] args) {
+		/* Ex08_콘솔입력연습예제를 참고해서 두 정수와 연산자를 입력해서 연산 결과를 출력하는 코드를 작성하세요.
+		 * - 연산자는 +, -, *, /, %만 처리
+		 * 
+		 * 예시
+		 * 정수1 입력 : 1 
+		 * 연산자 입력 : / (문자로 입력)
+		 * 정수2 입력 : 2
+		 * 1 / 2 = 0.5
+		 * 
+		 * 예시
+		 * 정수1 입력 : 1 
+		 * 연산자 입력 : ?
+		 * 정수2 입력 : 2
+		 * ?는 산술 연산자가 아닙니다.
+		 * */
+
+		//스캐너 만들기
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("정수를 입력하세요 : ");
+		int num1 = scan.nextInt();
+		
+		System.out.print("연산자를 입력하세요: ");
+		char op = scan.next().charAt(0);
+		
+		System.out.print("정수를 입력하세요 : ");
+		int num2 = scan.nextInt();
+		
+		//연산자 op가 +이면 num1 + num2를 콘솔에 출력
+		if(op == '+') {
+			System.out.println("" + num1 + op + num2 + "=" + (num1 + num2));
+		}
+		//연산자 op가 -이면 num1 - num2를 콘솔에 출력
+		else if(op == '-') {
+			System.out.println("" + num1 + op + num2 + "=" + (num1 - num2));
+		}
+		//연산자 op가 *이면 num1 * num2를 콘솔에 출력
+		else if(op == '*') {
+			System.out.println("" + num1 + op + num2 + "=" + (num1 * num2));
+		}
+		//연산자 op가 /이면 num1 / num2를 콘솔에 출력
+		else if(op == '/') {
+			System.out.println("" + num1 + op + num2 + "=" + (num1 / (double)num2));
+		}
+		//연산자 op가 %이면 num1 % num2를 콘솔에 출력
+		else if(op == '%') {
+			System.out.println("" + num1 + op + num2 + "=" + (num1 % num2));
+		}
+		//연산자 op가 산술연산자가 아니면 op는 산술연산자가 아닙니다를 콘솔에 출력
+		else {
+			System.out.println(op+"는 산술 연산자가 아닙니다.");
+		}
+	}
+
+}
