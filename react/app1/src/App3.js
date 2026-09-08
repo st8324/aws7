b03aaf53fece58838945f0860ea63c1264c3f6b0
st8324 <stajun@kut.ac.kr>
2026-09-08 17:08:04 +0900

    리액트 01일차 : 배열을 이용한 화면 구성 예제

--- react/app1/src/App3.js
+++ react/app1/src/App3.js
@@ -20,7 +20,10 @@ const datas = [
 		downCount : 1
 	}
 ]
-//배열을 이용하여 화면에 배치하는 예제
+// 배열을 이용하여 화면에 배치하는 예제
+// 기존 js는 문자열로된 html코드를 만든 후 innerHTML로 했음
+// 리액트에서는 배열의 map을 이용하여 화면 배치 
+
 function App3(){
 	const [posts, setPosts] = useState([]);
 	const loadDatas = ()=> setPosts(datas);
@@ -29,29 +32,31 @@ function App3(){
 			<button onClick={loadDatas}>게시글 불러오기</button>
 			<table border={1}>
 				<thead>
-					<th>번호</th>
-					<th>제목</th>
-					<th>작성자</th>
-					<th>작성일</th>
-					<th>조회수</th>
-					<th>추/비추</th>
+					<tr>
+						<th>번호</th>
+						<th>제목</th>
+						<th>작성자</th>
+						<th>작성일</th>
+						<th>조회수</th>
+						<th>추/비추</th>
+					</tr>
 				</thead>
 				<tbody>
 					{
-						posts.length == 0 ? 
+						posts.length === 0 ? 
 							<tr>
 								<th colSpan={6}>등록된 게시글이 없습니다.</th>
 							</tr> 
 							:
 							posts.map(post=>{
 								return (
-									<tr>
-										<td></td>
-										<td></td>
-										<td></td>
-										<td></td>
-										<td></td>
-										<td></td>
+									<tr key={post.num}>
+										<td>{post.num}</td>
+										<td>{post.title}</td>
+										<td>{post.memberId}</td>
+										<td>{post.createdAt}</td>
+										<td>{post.view}</td>
+										<td>{post.upCount}/{post.downCount}</td>
 									</tr>
 								)
 							})
