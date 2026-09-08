2914f32ced60c92bc111a22a84868f44a8b5c814
st8324 <stajun@kut.ac.kr>
2026-09-08 16:23:04 +0900

    리액트 01일차 : 컴포넌트,  이벤트 등록, props 예제

---
+++ react/app1/src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import App3 from './App3';
+// import {App2} from './App2';
+// import App from './App1';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // App이라는 컴포넌트
+  // 컴포넌트 특징 태그처럼 생김
+  // 시작을 대문자로 함
+  // <App />
+  // <App2 />
+  <App3 />
+);
